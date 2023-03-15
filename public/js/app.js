e6aeec332fbed9b1f4ceb062e851cdb0570f7112
thanhlk0803 <20211TT1281@mail.tdc.edu.vn>
2023-03-15 12:47:50 +0700

    upload

---
+++ public/js/app.js
@@ -0,0 +1,10 @@
+window._subiz_init_2094850928430 = true
+window._subiz_init_2094850928430_keep_loading = true
+var tag = document.createElement('script')
+tag.async = 1
+tag.src = 'https://vcdn.subiz-cdn.com/widget-v4/public/186c9849f9c.app.js'
+
+if (document.readyState === "interactive" || document.readyState === "complete") document.body.appendChild(tag)
+else document.addEventListener("DOMContentLoaded", function(event) {
+	document.body.appendChild(tag)
+}) // 186c9849f9c
